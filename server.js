381c85ef551eab8df90bcb1962882a44a8e3fbea
ksupuns <58617292+ksupuns@users.noreply.github.com>
2020-09-17 11:45:15 +0530

    20200917

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express=require('express');
-const cors=require('cors');
+const express=require('express');//to support sql
+const cors=require('cors');//resourse sharing
 
 const bodyParser=require('body-parser');
 
@@ -43,18 +43,93 @@ app.listen(PORT,()=>{
 app.get('/',(req,res)=>{
     res.send(`you are in default port`);
 });
-
-app.get('/add',(req,res)=>{
+/*
+app.post('/add',(req,res)=>{
     var id,name,nic,tp;
 
+    console.log(req);
     id=req.body.pid;
     name=req.body.pname;
     nic=req.body.pnic;
     tp=req.body.ptp;
 
 
-    console.log(id);
-    console.log(name);
-    console.log(nic);
-    console.log(tp);
-})
+    //console.log(id);
+    //console.log(name);
+    //console.log(nic);
+    //console.log(tp);
+});
+*/
+
+//Data Inserting
+app.post('/add', function(req,res){
+    var id,name,nic,tp;
+
+    console.log(req);
+    id=req.body.pid;
+    name=req.body.pname;
+    nic=req.body.pnic;
+    tp=req.body.ptp;
+
+    const InsertQuery = "insert into tbl_react values ('"+id+"','"+name+"','"+nic+"','"+tp+"')";
+    OracleDB.autoCommit=true,//only in insert,update,delete
+    OracleDB.getConnection({
+            user          : 'CICL',
+            password      : 'cicl',
+            connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "CICCO-SCAN/ceyt",
+          },
+
+
+          function(err, connection)
+          {
+            if (err) { console.error(err); return; }
+            connection.execute(InsertQuery , function(err, result)
+            {
+              if (err)
+              {
+                console.error(err);
+                return;
+              }
+              else
+              {
+                console.log("Success : Data Inserted");
+              }
+            });
+          });
+        });
+
+
+// data retrive        
+app.get('/view', function(req,res){
+    const selectQuery = "select * from  tbl_react";
+    OracleDB.getConnection({
+        user          : 'CICL',
+        password      : 'cicl',
+        connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "CICCO-SCAN/ceyt",
+      },
+
+
+      function(err, connection)
+      {
+        if (err) { console.error(err); return; }
+        connection.execute(selectQuery , function(err, result)
+        {
+          if (err)
+          {
+            console.error(err);
+            return;
+          }
+          else
+          {
+            //console.log("Success : Data Inserted");
+            return res.json({
+                result
+            });
+          }
+        });
+      });
+});
+// after this check browser
+//url -> localhost:4000/view
+// front end
+
